149e2579e1ea51954df7c86265edb880aed3a810
xscapex <monicahsuu@gmail.com>
2022-02-10 15:39:22 +0800

    update 台北101影片

--- app.py
+++ app.py
@@ -54,6 +54,10 @@ def index():
                                            getMRTVideoMessage()]
                 elif text == "台北101圖":
                     payload["messages"] = [getTaipei101ImageMessage()]
+
+                elif text == "台北101影片":
+                    payload["messages"] = [getMRTVideoMessage()]
+
                 elif text == "quoda":
                     payload["messages"] = [
                             {
@@ -216,6 +220,9 @@ def getTaipei101LocationMessage():
 
 def getMRTVideoMessage():
     message = dict()
+    message["type"] = "video"
+    message["originalContentUrl"] = F"{end_point}/static/taipei_101_video.mp4"
+    message["previewImageUrl"] = F"{end_point}/static/taipei_101.jpeg"
     return message
 
 
